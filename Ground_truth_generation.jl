42df9ae4fced9ccf904d94762831bd8532c553af
Jin XU <jin.xu.2010@gmail.com>
2020-05-04 11:23:37 -0700

    Update Ground_truth_generation.jl

--- Ground_truth_generation.jl
+++ Ground_truth_generation.jl
@@ -68,12 +68,7 @@ function randomNetwork(rr, nSpecies, nSpecies_gene, nRxns_limitation)
         ids_tot = [] #to check if all the species are involved in reactions
         global species_ids = collect(1:nSpecies)
         global non_gene_species_ids = collect((nSpecies_gene+1):nSpecies)
-
-        #sbmlFile = "minimal.xml"
-        #f = open(sbmlFile)
-        #sbmlStr = read(f,String)
-        #close(f)
-        #loadSBML(rr, sbmlStr)
+        
         addCompartment(rr, "compartment", 1.0, false)
 
         for s in species
